a3e2f905703b85ceec28b537ef5fad0ec7b13bfb
Igor Komlew <igor.komlew@gmail.com>
2026-04-10 22:40:44 +0200

    test(agent): use strings.Contains in context_crawler_test for stability

--- internal/agent/context_crawler_test.go
+++ internal/agent/context_crawler_test.go
@@ -3,6 +3,7 @@ package agent
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -103,14 +104,5 @@ func TestCrawlProjectContext_EmptyFile(t *testing.T) {
 }
 
 func contains(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr || len(s) > 0 && containsSubstr(s, substr))
-}
-
-func containsSubstr(s, substr string) bool {
-	for i := 0; i <= len(s)-len(substr); i++ {
-		if s[i:i+len(substr)] == substr {
-			return true
-		}
-	}
-	return false
+	return strings.Contains(s, substr)
 }
